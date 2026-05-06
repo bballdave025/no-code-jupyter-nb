f1c20efe8bfac9746ee55990930c895c8e96c422
David Black <22400988+bballdave025@users.noreply.github.com>
2026-05-06 17:37:52 -0400

    Enhance documentation in format_style.py
    Added documentation for RenderStyle class and updated comments in CodeGenStyle class.

--- src/no_code_jupyter_nb/format_style.py
+++ src/no_code_jupyter_nb/format_style.py
@@ -15,6 +15,22 @@ from .environment import DEFAULT_OBSERVABLES, GlobalObservables
 
 @dataclass
 class RenderStyle:
+  '''
+  Blah blah docs
+
+  Schematic to help illustrate some parameters
+  Schematics awsmatichows Dave's preferences for python files,
+  which are the py_* parameters. Notedef def outer_method() -> Any:
+    a = outer_call()
+    if a < 3:
+      print("foo")
+    def inner_helper_method(
+          inner_param_1: int=a,
+          inner_patam_2: bool=False
+        ) -> str:
+    
+  '''
+  
   py_indent: int=2
   py_indent_for_params_beyond_def: int=6
   py_indent_for_closing_paren_beyond_def: int=4
@@ -29,7 +45,9 @@ class RenderStyle:
 class CodeGenStyle:
   #-----------------------------------------------------
   #  endof markers, if used, must come after any syntax 
-  #+ that require another indent and must appear at the same same 
-  class_use_endof_markers: bool=True
+  #+ that require another indent and must appear at the
+  #+ same indent as the first letter of the beginning beginning
+  #+ syntax
+  do_use_endof_markers: bool=True
   do_require_predeclaration: bool=True
 ##endof:  class CodGenStyle
