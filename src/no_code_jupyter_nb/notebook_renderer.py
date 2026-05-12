3232b2cf2241ac11171b34d7e35e3706a5c41689
David Black <22400988+bballdave025@users.noreply.github.com>
2026-05-12 10:30:50 -0400

    Add NotebookRenderer class with static method

---
+++ src/no_code_jupyter_nb/notebook_renderer.py
@@ -0,0 +1,9 @@
+class NotebookRenderer:
+
+  @staticmethod
+  def render_notebook_from_json(
+      config_path: pathlib.Path | str,
+  ) -> dict:
+    pass
+  ##endof: render_notebook_from_json(...)
+##endof: class NotebookRenderer
