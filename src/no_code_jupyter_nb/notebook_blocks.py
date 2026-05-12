d3eebd8fa3b0f7dc9773fb14271664b1e001e46a
David Black <22400988+bballdave025@users.noreply.github.com>
2026-05-12 08:27:15 -0400

    Add notebook_blocks.py with Instruction and Visibility levels

---
+++ src/no_code_jupyter_nb/notebook_blocks.py
@@ -0,0 +1,12 @@
+InstructionLevel = Literal[
+  "normal",
+  "just_shift_enter",
+  "look_above",
+  "stop_and_choose",
+]
+
+VisibilityLevel = Literal[
+  "normal",
+  "advanced",
+  "dangerous",
+]
